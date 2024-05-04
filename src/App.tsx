9626a4d377faccf31446079a97f961bb3809abae
GJ Tiquia <gershom.tiquia@gmail.com>
2024-05-05 00:24:54 +0800

    feat: basic styling with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
 
 function App() {
+
+    const [password, setPassword] = useState("")
+
     return (
-        <h1>Hello World!</h1>
+        <div className="h-dvh flex flex-col justify-center items-center gap-4 p-4 w-full max-w-screen-sm">
+            <h1 className="font-bold text-2xl sm:text-3xl text-white">Mini Text Encryptor</h1>
+
+            <div className="flex gap-2">
+                <p className="text-white">Password:</p>
+                <input type="password" className="rounded-md px-2" value={password} onChange={e => setPassword(e.target.value)} />
+            </div>
+
+            <div className="w-full flex-grow flex flex-col gap-2">
+                <h2 className="text-white font-bold">Raw Text:</h2>
+                <textarea className="rounded-md w-full flex-grow p-2 resize-none" />
+            </div>
+
+            <div className="w-full flex-grow flex flex-col gap-2">
+                <h2 className="text-white font-bold">Encrypted Text:</h2>
+                <textarea className="rounded-md w-full flex-grow p-2 resize-none" />
+            </div>
+        </div>
     )
 }
 
