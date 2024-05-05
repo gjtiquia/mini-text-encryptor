a2d44804052226aca83d7bddb27d627e748d9aae
GJ Tiquia <gershom.tiquia@gmail.com>
2024-05-05 11:25:02 +0800

    WIP: hash and encrypt

--- src/InnerApp.tsx
+++ src/InnerApp.tsx
@@ -1,6 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRef } from "react";
 
+async function sha256Async(message: string) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string                  
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+// TODO 
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt
+// https://github.com/mdn/dom-examples/blob/main/web-crypto/encrypt-decrypt/rsa-oaep.js
+
 export function InnerApp() {
 
     const inputPasswordRef = useRef<HTMLInputElement>(null);
@@ -17,8 +36,10 @@ export function InnerApp() {
             const inputPassword = inputPasswordRef.current.value;
             const inputText = inputTextRef.current.value;
 
+            const passwordHash = await sha256Async(inputPassword);
+
             // TODO : Async encrypt
-            const encryptedText = inputText + inputPassword;
+            const encryptedText = inputText + passwordHash;
 
             return { output: encryptedText };
         },
