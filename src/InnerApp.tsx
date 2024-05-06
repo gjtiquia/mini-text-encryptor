e4d3743cb9d945be89865d3ec6f4464cab6db343
GJ Tiquia <gershom.tiquia@gmail.com>
2024-05-07 02:15:25 +0800

    feat: figured out AES encryption and decryption

--- src/InnerApp.tsx
+++ src/InnerApp.tsx
@@ -1,24 +1,102 @@
 import { useQuery } from "@tanstack/react-query";
 import { useRef } from "react";
 
-async function sha256Async(message: string) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder().encode(message);
+async function sha256ToHexStringAsync(message: string) {
+    const hashBuffer = await sha256ToArrayBufferAsync(message);
+    const hashHex = arrayBufferToHexString(hashBuffer)
+    return hashHex;
+}
 
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-
+function arrayBufferToHexString(buffer: ArrayBuffer) {
     // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashArray = Array.from(new Uint8Array(buffer));
 
     // convert bytes to hex string                  
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
     return hashHex;
 }
 
+async function sha256ToArrayBufferAsync(message: string) {
+    // encode as UTF-8
+    const msgBuffer = getMessageEncoding(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    return hashBuffer;
+}
+
 // TODO 
 // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt
-// https://github.com/mdn/dom-examples/blob/main/web-crypto/encrypt-decrypt/rsa-oaep.js
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/importKey
+// https://github.com/mdn/dom-examples/blob/main/web-crypto/encrypt-decrypt/aes-gcm.js
+// https://github.com/mdn/dom-examples/blob/main/web-crypto/import-key/raw.js
+
+function importSecretKeyAsync(rawKey: ArrayBuffer) {
+    return window.crypto.subtle.importKey(
+        "raw",
+        rawKey,
+        "AES-GCM",
+        true,
+        ["encrypt", "decrypt"]
+    );
+}
+
+function getMessageEncoding(message: string) {
+    const enc = new TextEncoder();
+    return enc.encode(message);
+}
+
+async function encryptMessageAsync(password: string, message: string) {
+
+    const keyBuffer = await sha256ToArrayBufferAsync(password);
+    const secretKey = await importSecretKeyAsync(keyBuffer)
+    const encodedMessage = getMessageEncoding(message);
+
+    // constant iv
+    // not recommended but this is so that can keep using the same passphrase
+    const iv = new Uint8Array(12);
+
+    const ciphertextBuffer = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-GCM",
+            iv: iv
+        },
+        secretKey,
+        encodedMessage
+    );
+
+    const ciphertext = arrayBufferToHexString(ciphertextBuffer);
+    return { ciphertextBuffer, ciphertext };
+}
+
+// TODO : find a way to convert string to ArrayBuffer
+// TODO : I think in one of the code examples above, hv one that does that
+// TODO : .buffer doesnt work, need to find the longer way to do so https://stackoverflow.com/questions/37228285/uint8array-to-arraybuffer
+async function decryptMessageAsync(password: string, ciphertextBuffer: ArrayBuffer) {
+
+    const keyBuffer = await sha256ToArrayBufferAsync(password);
+    const secretKey = await importSecretKeyAsync(keyBuffer)
+    // const encodedCiphertext = getMessageEncoding(ciphertext);
+
+    // constant iv
+    // not recommended but this is so that can keep using the same passphrase
+    const iv = new Uint8Array(12);
+
+    let decryptedBuffer = await window.crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: iv
+        },
+        secretKey,
+        ciphertextBuffer
+    );
+
+    let decoder = new TextDecoder();
+    const message = decoder.decode(decryptedBuffer);
+
+    return message;
+}
 
 export function InnerApp() {
 
@@ -36,12 +114,14 @@ export function InnerApp() {
             const inputPassword = inputPasswordRef.current.value;
             const inputText = inputTextRef.current.value;
 
-            const passwordHash = await sha256Async(inputPassword);
+            const { ciphertextBuffer, ciphertext } = await encryptMessageAsync(inputPassword, inputText);
 
-            // TODO : Async encrypt
-            const encryptedText = inputText + passwordHash;
+            // TODO : testing decryption
+            const decryptedText = await decryptMessageAsync(inputPassword, ciphertextBuffer);
+            // const decryptedText = await decryptMessageAsync("123", ciphertextBuffer); // TODO : Wrong password handling. Cuz... it freezes lol
+            console.log("decrypted:", decryptedText);
 
-            return { output: encryptedText };
+            return { output: ciphertext };
         },
     })
 
